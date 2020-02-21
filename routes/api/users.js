c60184984d3f6a2d6428c484897b9e4fe22c089a
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-02-21 11:44:10 -0500

    Add partially complete authentication routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const validateSignupInput = require("../../validation/signup");
+const validateLoginInput = require("../../validation/login");
+const bcrypt = require("bcryptjs");
 
-router.get('/test', (req, res) => res.json({ msg: 'This is the users route'}));
+router.post("/signup", (req, res) => {
+    const { errors, isValid } = validateSignupInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    User.findOne({ email: req.body.email }).then(user => {
+        if (user) {
+            errors.email = "User already exists";
+            return res.status(400).json(errors);
+        } else {
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            });
+
+        }
+    })
+    
+})
+
+router.post("/login", (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email }).then(user => {
+        if (!user) {
+            errors.email = "User with this email does not exist";
+            return res.status(400).json(errors);
+        }
+
+    })
+})
 
 module.exports = router;
