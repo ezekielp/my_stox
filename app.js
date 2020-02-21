31fe9f71e7f4402878284018366a74fd4dbd8afe
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-02-21 12:15:45 -0500

    Create passport file for JSON web token authentication

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const app = express();
 const db = require("./config/keys").mongoURI;
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
+const passport = require("passport");
+const passportAuth = require("./config/passport");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-
-app.get("/", (req, res) => res.send("Hello, world!"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
@@ -21,3 +21,8 @@ app.use("/api/users", users);
 app.use("/api/transactions", transactions);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(passport.initialize());
+
+
+
