5b1ad7cf3b334721c43a99a82f614229db6291a2
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-02-24 11:39:00 -0500

    Create GET, POST routes for transactions

--- routes/api/transactions.js
+++ routes/api/transactions.js
@@ -1,10 +1,52 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const passport = require('passport');
 
 const Transaction = require('../../models/Transaction');
-const validateTransactionInput = require();
+const validateTransactionInput = require('../../validation/transaction');
 
+const _normalized = transactions => {
+  const normalizedObj = {};
+  transactions.forEach(transaction => {
+    normalizedObj[transaction._id] = transaction;
+  });
+  return normalizedObj;
+}
+
+// An index of user transactions
+router.get(
+  "/:user_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Transaction.find({ user: req.params.user_id })
+      .then(transactions => res.json(_normalized(transactions)))
+      .catch(err =>
+        res
+          .status(404)
+          .json({ noTransactionsFound: "No transactions found for this user" })
+      );
+  }
+);
+
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateTransactionInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const { user, companyName, tickerSymbol, numberOfShares, shareValueAtTimeOfPurchase } = req.body;
+
+    const newTransaction = new Transaction({
+      user, companyName, tickerSymbol, numberOfShares, shareValueAtTimeOfPurchase
+    })
+
+    newTransaction.save().then(transaction => res.json(transaction));
+  }
+);
 
 module.exports = router;
