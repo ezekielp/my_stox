637becbbc5ab0b710f976255245fa626cf66fa53
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-02-24 09:46:47 -0500

    Create skeleton for navbar component, add routes to portfolio and transactions components

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -3,10 +3,14 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 
 import LandingPage from './landing_page/landing_page';
+import PortfolioIndexContainer from '../components/portfolio/portfolio_index_container';
+import TransactionsIndexContainer from '../components/transactions/transactions_index_container';
 
 const App = () => {
     return (
         <Switch>
+            <ProtectedRoute path="/portfolio" component={PortfolioIndexContainer} />
+            <ProtectedRoute path="/transactions" component={TransactionsIndexContainer} />
             <AuthRoute path="/" component={LandingPage} />
         </Switch>
     )
--- frontend/src/components/nav/navbar.jsx
+++ frontend/src/components/nav/navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 
 class Navbar extends React.Component {
@@ -9,7 +9,23 @@ class Navbar extends React.Component {
 
     render() {
         return (
-            <div>Hello from the navbar component!</div>
+
+            <ul className="navbar-ul">
+                <li></li>
+                <li>
+                    <Link to="/portfolio">
+                        Portfolio
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/transactions">
+                        Transactions
+                    </Link>
+                </li>
+                <li>Logout</li>
+            </ul>
+
+
         )
     }
 }
